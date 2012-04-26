ae4a6f2feec72ea25b66c7ec126527d8ed602ff7
Ian Vaughan <github@ianvaughan.co.uk>
2012-04-26 17:55:48 +0100

    Add validation checks for command line params

--- lib/tabelr.rb
+++ lib/tabelr.rb
@@ -6,6 +6,8 @@ module Tabelr
 
     def run
       args = parse_args
+      return unless valid args
+
       @tf = TableFormator.new
 
       json = JSON.parse(File.read(args[0]))
@@ -21,6 +23,18 @@ module Tabelr
         commands << arg unless arg.index('-') === 0
       end
       commands
+    end
+
+    def valid args
+      if args[0].nil?
+        puts "Man, you gotta give me a file to read from!"
+        return
+      end
+      unless File.exist?(args[0])
+        puts "Man, you gotta give me a valid file to read from!"
+        return
+      end
+      true
     end
 
   end
